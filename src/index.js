8e834b8068f8f91ea4cf50523d8edf4f05027e98
Max <max@maxshvedov.ru>
2017-06-28 18:03:24 +0300

    add all files from yard-frontend

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
